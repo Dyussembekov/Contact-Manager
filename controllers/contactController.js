9c2040bfb1df67b468b4ca8c1d0a159ae7e24fa8
Dyussembekov <ndyusembekov07@gmail.com>
2023-03-21 17:03:23 +0600

    added controllers and db

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require("express-async-handler")
+const Contact = require("../models/contactModel")
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
 const getAllContacts = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get all contacts" })
+    const contacts = Contact.find()
+    res.status(200).json(contacts)
 })
 
 //@desc Create New contact
@@ -16,18 +18,28 @@ const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is :", req.body);
     const {name, email, phone} = req.body
     if (!name || !email || !phone) {
-        res.status(400)
+        res.status(400)  
         throw new Error("All fields are mandatory")
     }
-    res.status(201).json({ message: "Create contact" })
-})
+    const contact = await Contact.create({
+        name, 
+        email,
+        phone
+    })
+    res.status(201).json(contact)
+}) 
 
 //@desc Get contact
 //@route GET /api/contacts
 //@access public
 
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get contact" })
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    res.status(200).json(contact)
 })
 
 
@@ -36,7 +48,17 @@ const getContact = asyncHandler(async (req, res) => {
 //@access public
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Update Contact" })
+    const contact = await Contact.findById(req.params.id)
+    if (!contact) {
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    )
+    res.status(200).json(updatedContact)
 })
 
 //@desc Delete contact
@@ -44,8 +66,18 @@ const updateContact = asyncHandler(async (req, res) => {
 //@access public
 
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Delete Contact" })
-})
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+      res.status(403);
+      throw new Error("User don't have permission to update other user contacts");
+    }
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(contact);
+  });
 
 
 
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require("express")
-const app = express()
-const dotenv = require("dotenv").config()
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
-const errorHandler = require("./middleware/errorHandler")
+connectDb();
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.get("/api/contacs", require("./routes/contactRoutes"))
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
